4f5d1fd193124ac49374a0d191dae042e94a8324
Tatyana <breatninsideme@gmail.com>
2023-09-03 19:49:54 +0400

    add scoring

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const rex = document.getElementById("rex");
 const cactus = document.getElementById("cactus");
 
+let score = 0;
+document.getElementById("score").innerHTML = "score: " + score;
+
 document.addEventListener("keydown", function(event) {
     jump();
 });
@@ -15,11 +18,27 @@ function jump() {
         
 }
 
+
+
+function addScore() {
+    score+=1;
+    document.getElementById("score").innerHTML = "score: " + score;
+}
+
+let isScore = setInterval (function() {
+    let rexTop = parseInt(window.getComputedStyle(rex).getPropertyValue("top"));
+    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+    if (cactusLeft < 50 && cactusLeft > 0 && rexTop < 140) {
+        addScore();
+    }
+}, 200)
+
 let isAlive = setInterval ( function() {
     let rexTop = parseInt(window.getComputedStyle(rex).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
-
     if (cactusLeft < 50 && cactusLeft > 0 && rexTop >= 140) {
-        alert("GAME OVER")
+        score = 0;
+        document.getElementById("score").innerHTML = "score: " + score;
+        alert("GAME OVER");
     }
 }, 10)
